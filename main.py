b8bf54e38aba068f3b022d37ec312974a99a7ab1
HoChanGwak <khc9154@naver.com>
2022-08-29 00:28:52 +0900

    결석인원출력(로딩속도는 현저히감소)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import flask
 import time
 import facenet
 import detect_face
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 import pickle
 from PIL import Image
 from werkzeug.utils import secure_filename
@@ -26,10 +26,6 @@ modeldir = './model/20180402-114759.pb'
 classifier_filename = './class/classifier.pkl'
 npy='./npy'
 train_img="./train_img"
-
-
-
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Tensorflow object detection API')
@@ -60,44 +56,13 @@ app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg','mp4'])
 def upload():
     return render_template('upload.html')
 
-def run_model(video):
+# DRAW_REC : 바운딩 박스를 그리고싶으면 True
+def run_model(video, DRAW_BOX):
     frame_list = []
-    # cap = cv2.VideoCapture(video)
-    # width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    # height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    # fps = cap.get(cv2.CAP_PROP_FPS)
-    # time.sleep(0.2)
-    # lastTime = time.time()*1000.0
-
-    # while True:
-    #     ret, image = cap.read()
-    #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-       
-
-    #     delt = time.time()*1000.0-lastTime
-    #     s = str(int(delt))
-    #     lastTime = time.time()*1000.0
-
-
-    #     # cv2.putText(image, s, (10, 25),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-    #     # now = datetime.datetime.now()
-    #     # timeString = now.strftime("%Y-%m-%d %H:%M")
-    #     # cv2.putText(image, timeString, (10, 45),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-    #     cv2.imshow("Frame", image)
-    #     key = cv2.waitKey(1) & 0xFF
-    #  # if the `q` key was pressed, break from the loop
-    #     if key == ord("q"):
-    #         break
-   
-    #     ret, buffer = cv2.imencode('.jpg', image)
-    #     frame = buffer.tobytes()
-    #     yield (b'--frame\r\n'
-    #            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     
-   
     with tf.Graph().as_default():
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
-        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.6)
+        sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
             pnet, rnet, onet = detect_face.create_mtcnn(sess, npy)
             minsize = 30  # minimum size of face
@@ -111,9 +76,9 @@ def run_model(video):
             HumanNames.sort()
             print('Loading Model')
             facenet.load_model(modeldir)
-            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+            images_placeholder = tf.compat.v1.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.compat.v1.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.compat.v1.get_default_graph().get_tensor_by_name("phase_train:0")
             embedding_size = embeddings.get_shape()[1]
             classifier_filename_exp = os.path.expanduser(classifier_filename)
             with open(classifier_filename_exp, 'rb') as infile:
@@ -121,16 +86,36 @@ def run_model(video):
             
             video_capture = cv2.VideoCapture(video)
             print('Start Recognition')
-            a = set(HumanNames)
-            tm = time.time()
-            while True:
-                ret, frame = video_capture.read()
+            # 학생 이름(set)
+            member_names = set(HumanNames)
+            # 출석부(list)
+            attendance_list = []
             
-                #frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)    #resize frame (optional)
-                timer =time.time()
+            tm = time.time()
+            
+            # 비디오 총 프레임 수 구하기
+            vid_length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+            
+            # 프레임 수 만큼 반복
+            for frame_cnt in range(vid_length):
+                # 프레임마다 추론하기 때문에 프레임수에 제한을 둔다.
+                # yu영상을 예로들면 영상은 약 33,030프레임이므로 서비스하기 쉽지 않다.
+                if frame_cnt > 900:
+                    break
+                # 서비스 속도의 개선을 위해 영상을 출력하지 않을 때(DRAW_BOX=False) 
+                # 프레임을 몇개씩 스킵해서 detection하도록 한다.
+                if (not DRAW_BOX) & (frame_cnt % 3 != 0):
+                    continue
+                
+                ret, frame = video_capture.read()
+                
                 if frame.ndim == 2:
                     frame = facenet.to_rgb(frame)
+                
+                # 결국 이 한줄이 서비스가 오래걸리는 이유인데..
+                # 함수의 detect_face함수의 조정으로 개선이 필요해보임(물론 실제로 서비스할때 개선이 필요한 것)
                 bounding_boxes, _ = detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+                
                 faceNum = bounding_boxes.shape[0]
                 if faceNum > 0:
                     det = bounding_boxes[:, 0:4]
@@ -160,7 +145,6 @@ def run_model(video):
                             emb_array[0, :] = sess.run(embeddings, feed_dict=feed_dict)
                             predictions = model.predict_proba(emb_array)
                             best_class_indices = np.argmax(predictions, axis=1)
-                        
                             
                             best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
                             if best_class_probabilities>0.87:
@@ -169,73 +153,80 @@ def run_model(video):
                                 for H_i in HumanNames:
                                     if HumanNames[best_class_indices[0]] == H_i:
                                         result_names = HumanNames[best_class_indices[0]]
+                                        # 점찍는 print 때문에 줄바꿈용도로 작성
+                                        print()
                                         print("Predictions : [ name: {} , accuracy: {:.3f} ]".format(HumanNames[best_class_indices[0]],best_class_probabilities[0]))
-                                        cv2.rectangle(frame, (xmin, ymin-20), (xmax, ymin-2), (0, 255,255), -1)
-                                        cv2.putText(frame, result_names, (xmin,ymin-5), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                                    1, (0, 0, 0), thickness=1, lineType=1)
-                                        a = a - set(result_names.split())
-                                        
+                                        if DRAW_BOX:
+                                            cv2.rectangle(frame, (xmin, ymin-20), (xmax, ymin-2), (0, 255,255), -1)
+                                            cv2.putText(frame, result_names, (xmin,ymin-5), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                                        1, (0, 0, 0), thickness=1, lineType=1)
+                                        # 출석자는 출석부에 기록됨
+                                        attendance_list.append(HumanNames[best_class_indices[0]])
                                         
                             else :
-                                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-                                cv2.rectangle(frame, (xmin, ymin-20), (xmax, ymin-2), (0, 255,255), -1)
-                                cv2.putText(frame, "?", (xmin,ymin-5), cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                                                    1, (0, 0, 0), thickness=1, lineType=1)
-                        except:   
-                            
+                                if DRAW_BOX:
+                                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                                    cv2.rectangle(frame, (xmin, ymin-20), (xmax, ymin-2), (0, 255,255), -1)
+                                    cv2.putText(frame, "?", (xmin,ymin-5), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                                        1, (0, 0, 0), thickness=1, lineType=1)
+                        except:
                             print("error")
                         
                 endtimer = time.time()
-                fps = 1/(endtimer-timer)
-                # cv2.rectangle(frame,(15,30),(135,60),(0,255,255),-1)
-                # cv2.putText(frame, "fps: {:.2f}".format(fps), (20, 50),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-                cv2.imshow('Face Recognition', frame)
-                key = cv2.waitKey(30)
-                print(endtimer-tm,'111')
-                if (endtimer - tm) >12:
-                    if len(list(a)) == 0:
-                        member_check = '전원출석'
-                        print(member_check)
-                    else:
-                        print('결석 인원 : ',end='')
-                        for member_check in list(a):
-                            print(member_check,end=' ')
-                        member_check = '결석 인원 :' + ' '.join(list(a))
+                # 너무 많아서 주석처리
+                # print('[time log]now-time', endtimer-tm)
+                print('.', end = '')
 
-                   
-                    break
-                # key= cv2.waitKey(1)
-                # if key== 113: # "q"
-                #     #직접 작성스타트
-                #     # print(result_names,'aaa')
-                #     if len(list(a)) == 0:
-                #         print('전원출석')
-                #     else:
-                #         print('결석 인원 : ',end='')
-                #         for i in list(a):
-                #             print(i,end=' ')
-                
-                #     break
-            
                 ret, buffer = cv2.imencode('.jpg', frame)
                 frame1 = buffer.tobytes()
+                # 출력할 프레임 저장
                 frame_list.append(frame1)
+                
+                
+    # 영상 종료시 페이지에 출력할 데이터 확인
+    # 출석하지 않은 사람은 기억해둔다.
+    absent_names = list(set(HumanNames).difference(set(attendance_list)))
+    
+    if len(absent_names) == 0:
+        member_check = '전원출석'
+        print(f'[yolo log] 결석자 이름 : {absent_names}')
+        print(f'[yolo log] 출력 결과 : {member_check}')
+        
+    else:
+        member_check = '결석 인원 : '
+        print(f'[yolo log] 결석 인원 : {absent_names}')
+        for name in absent_names:
+            member_check = member_check + name + ' '
             
-            return frame_list, member_check
+    return frame_list, member_check
                 
 
 def result_frames(video):
-    frame_list, _ = run_model(video)
-    for frame1 in frame_list:
+    frame_list, _ = run_model(video, DRAW_BOX=True)
+    frame_cnt = -1
+    #### 영상 1회재생 ####
+    # for frame in frame_list: 
+    #     frame_cnt = frame_cnt + 1
+    #     frame = frame_list[frame_cnt]
+        #### 영상 1회재생 ####
+    
+    #### 영상 반복재생 ####
+    
+    while True: 
+        frame_cnt = frame_cnt + 1
+        if frame_cnt >= len(frame_list):
+            frame_cnt = 0
+        frame = frame_list[frame_cnt]
+        #### 영상 반복재생 ####
+        
+        # 영상은 초당 약 30프레임이므로 프레임마다 0.03초 마다 출력(데이터 전송)
+        time.sleep(0.03)
         yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame1 + b'\r\n')
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 def cal_attendance(video):
-    _, member_check = run_model(video)
+    _, member_check = run_model(video, DRAW_BOX=False)
     return member_check
-
-
-
 
 #디텍션 처리
 @app.route('/upload_process', methods=['POST'])
@@ -245,45 +236,7 @@ def upload_process():
     file.save(os.path.join('static/image/', filename))
     print(filename,8969)
     y = cal_attendance(f'static/image/{filename}')
-    
-    # nparr = np.frombuffer(file.read(), np.uint8)
-    # image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # filename_first = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
-
-    # dst = tflite_catdog.run(image)
-
-    # json_path = os.path.join('outputs', filename_first + '.json')
-    # json.dump({'rate':float(dst)},open(json_path,'w'))
-    
-    # dst_path = os.path.join('outputs', filename_first + '.jpg')
-    # cv2.imwrite(dst_path, image)
-    
-    # return redirect(url_for('result',filename=filename,y=y))
     return render_template("result.html",filename=filename,y=y)
-
-# #outputs 폴더를 일반 웹서버 형식으로 오픈
-# @app.route('/outputs/<path:filename>', methods=['GET', 'POST'])
-# def download(filename):
-#     output_path = os.path.join(app.root_path, 'outputs')
-#     return send_from_directory('outputs', filename)
-
-#디텍션 결과 보여주기
-# @app.route('/result')
-# def result():
-#     filename = request.args.get('filename')
-#     y = request.args.get('y')
-#     print(y,7770)
-#     z = result_frames(f'static/image/{filename}')
-#     # for i in z:
-#     #     print(i,'log')
-#     #     break
-#     # print(x,890)
-#     # print(z.__next__,888)
-#     print(z,5673)
-#     # return Response(result_frames(),mimetype='multipart/x-mixed-replace;boundary=frame')
-#     # return render_template("result.html", file_name = "image/park.mp4")
-#     return render_template("result.html",filename=filename,y=z)
 
 @app.route('/result_final')
 def result_final():
@@ -294,49 +247,12 @@ def result_final():
 
 @app.route('/team')
 def team():
-
     return render_template("team.html")
+
 @app.route('/login')
 def login():
-    
     return render_template("login.html")
 
-# # API 서비스
-# @app.route('/object_detection', methods=['POST'])
-# def infer():
-#     nparr = np.frombuffer(request.data, np.uint8)
-#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-#     rects, classes, scores = tflite_detector.inference(img)
-
-#     result = {'rects':rects.tolist(), 'classes':classes.tolist(), 'scores':scores.tolist()}
-#     response = json.dumps(result)
-
-#     return Response(response=response, status=200, mimetype="application/json")
-
-# import io
-# # API 서비스
-# @app.route('/color_service', methods=['POST'])
-# def color_service():
-#     nparr = np.frombuffer(request.data, np.uint8)
-#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-#     dst = tflite_color.run(img)
-
-#     _, buf = cv2.imencode('.jpg', dst)
-#     return flask.send_file(io.BytesIO(buf), download_name='result.jpg', mimetype='image/jpeg')
-
-
-
-# start flask app
-# def main():
-#     # os.makedirs('outputs', exist_ok=True)
-#     # tflite_detector.load_model("detect.tflite")
-#     # tflite_catdog.load_model('dogcat.tflite')
-#     args = parse_args()
-#     app.run(host=args.host, port=args.port, debug=args.debug)
-
 if __name__ == "__main__":
-    # main()
     args = parse_args()
     app.run(host=args.host, port=args.port, debug=args.debug)
